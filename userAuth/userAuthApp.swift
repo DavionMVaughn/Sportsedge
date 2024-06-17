4e61c001a51c81fc194f995344e80238061c8648
Davion <Davion.vaughn@hgs.hiddengeniusproject.org>
2024-06-17 12:06:39 -0400

    Initial Commit

---
+++ userAuth/userAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  userAuthApp.swift
+//  userAuth
+//
+//  Created by Davion Vaughn on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct userAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
